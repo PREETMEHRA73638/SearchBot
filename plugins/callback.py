bd4d9b4aebc6106626520be323e02dd72236b1ae
PREETMEHRA73638 <109457025+PREETMEHRA73638@users.noreply.github.com>
2023-08-27 00:41:06 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -119,7 +119,7 @@ async def button(bot, cmd: CallbackQuery):
             ], 
                                         [
                         InlineKeyboardButton("How To Watch?", callback_data="Watch_msg"),
-                        InlineKeyboardButton('Support', url='http://t.me/messagecontactbot')
+                        InlineKeyboardButton('Support', url='https://t.me/update_channel_738')
                     ]
                 ]
             ),
@@ -189,7 +189,7 @@ async def button(bot, cmd: CallbackQuery):
             ], 
                                         [
                         InlineKeyboardButton("Terabox", callback_data="Terabox_msg"),
-                        InlineKeyboardButton('Watch Video', url='https://t.me/CyniteBackup/17')
+                        InlineKeyboardButton('Watch Video', url='https://t.me/update_channel_738/5')
                     ],[
                         InlineKeyboardButton("Back", callback_data="Watch_msg")
                     ]
